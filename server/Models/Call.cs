be4e1c3344354eb8024cfeb5797e91b3e06c1876
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-24 22:07:43 +0300

    Added Migrations To The Database

--- server/Models/Call.cs
+++ server/Models/Call.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using server.Abstracts;
 
 namespace server.Models
@@ -10,12 +11,18 @@ namespace server.Models
         public DateTime MadeOn { get; set; }
         public bool IsCompleted { get; set; }
         public CallType Type { get; set; }
-        public int CreatedById { get; set; }
-        public Admin CreatedBy { get; set; } = new Admin();
         public DateTime CreatedOn { get; set; }
-        public int? UpdatedById { get; set; }
-        public Admin? UpdatedBy { get; set; }
         public DateTime? UpdatedOn { get; set; }
+
+        [ForeignKey("CreatedBy")]
+        public string CreatedById { get; set; } = string.Empty;
+        public Admin CreatedBy { get; set; } = new Admin();
+
+        [ForeignKey("UpdatedBy")]
+        public string? UpdatedById { get; set; }
+        public Admin? UpdatedBy { get; set; }
+
+        [ForeignKey("Client")]
         public int? ClientId { get; set; }
         public Client? Client { get; set; }
     }
--- server/Models/Client.cs
+++ server/Models/Client.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace server.Models
 {
     public class Client
@@ -6,10 +8,16 @@ namespace server.Models
         public string Name { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public Admin CreatedBy { get; set; } = new Admin();
         public DateTime CreatedOn { get; set; }
-        public Admin? UpdatedBy { get; set; }
         public DateTime? UpdatedOn { get; set; }
         public List<Call> Calls { get; set; } = [];
+
+        [ForeignKey("CreatedBy")]
+        public string CreatedById { get; set; } = string.Empty;
+        public Admin CreatedBy { get; set; } = new Admin();
+
+        [ForeignKey("UpdatedBy")]
+        public string? UpdatedById { get; set; }
+        public Admin? UpdatedBy { get; set; }
     }
 }
