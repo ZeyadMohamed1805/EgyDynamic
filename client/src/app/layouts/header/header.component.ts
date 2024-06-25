4db71a4c49eed9f5e8e7ceb025d2d42690c25e8a
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-26 01:08:59 +0300

    Completed The Header Component's Template

--- client/src/app/layouts/header/header.component.ts
+++ client/src/app/layouts/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatMenuModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
