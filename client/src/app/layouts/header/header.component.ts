3fc02ef478453764f9ab0c7c5e314624527075f7
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-26 01:11:47 +0300

    Completed The Logout Process

--- client/src/app/layouts/header/header.component.ts
+++ client/src/app/layouts/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
+import { Router } from '@angular/router';
+import { StorageService } from '../../services/storage/storage.service';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +10,14 @@ import { MatMenuModule } from '@angular/material/menu';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(
+    private readonly router: Router,
+    private readonly storageService: StorageService
+  ) {}
+
+  onLogout(): void {
+    this.storageService.removeItem('EGD_TOKEN');
+    this.router.navigateByUrl('auth');
+  }
+}
