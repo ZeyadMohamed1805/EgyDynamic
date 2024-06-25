42b112026df7838cf50545988e3611fb744aee52
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-25 23:50:22 +0300

    Completed Activating Layout Based On Routes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: '<router-outlet />',
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FooterComponent],
+  templateUrl: 'app.component.html',
+  styleUrl: 'app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  isLayoutVisible: boolean = false;
+  routesThatActivateLayout: string[] = ['/dashboard'];
+
+  constructor(private readonly router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((route: any) => {
+      this.isLayoutVisible = this.routesThatActivateLayout.includes(route.url);
+    });
+  }
+}
