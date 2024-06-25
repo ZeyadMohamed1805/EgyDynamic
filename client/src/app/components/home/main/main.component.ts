11ba1689a8d0367f6bf67b940f12ce7585dd3116
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-25 08:35:49 +0300

    Add The Main Components To Their Pages

--- client/src/app/components/home/main/main.component.ts
+++ client/src/app/components/home/main/main.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, WritableSignal, signal } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  dots: WritableSignal<number> = signal(1);
 
+  constructor(private readonly router: Router) {}
+
+  ngOnInit(): void {
+    setTimeout(() => this.router.navigateByUrl('auth'), 3000);
+    this.animateDots();
+  }
+
+  animateDots(): void {
+    setInterval(() => this.updateDots(), 200);
+  }
+
+  updateDots(): void {
+    this.dots.update((value: number) => (value === 3 ? 1 : value + 1));
+  }
 }
--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,28 +1,10 @@
-import { Component, OnInit, WritableSignal, signal } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { MainComponent } from '../../components/home/main/main.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  imports: [MainComponent],
+  template: '<app-main />',
 })
-export class HomeComponent implements OnInit {
-  dots: WritableSignal<number> = signal(1);
-
-  constructor(private readonly router: Router) {}
-
-  ngOnInit(): void {
-    setTimeout(() => this.router.navigateByUrl('auth'), 3000);
-    this.animateDots();
-  }
-
-  animateDots(): void {
-    setInterval(() => this.updateDots(), 200);
-  }
-
-  updateDots(): void {
-    this.dots.update((value: number) => (value === 3 ? 1 : value + 1));
-  }
-}
+export class HomeComponent {}
