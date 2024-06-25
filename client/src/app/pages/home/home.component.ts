30966bb7c23f885f5b38a1cf70635f9a2cca057d
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-25 06:50:36 +0300

    Initially Completed The Home Page

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, WritableSignal, signal } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  dots: WritableSignal<number> = signal(1);
 
+  constructor(private readonly router: Router) {}
+
+  ngOnInit(): void {
+    setTimeout(() => this.router.navigateByUrl('auth'), 3000);
+    this.animateDots();
+  }
+
+  animateDots(): void {
+    setInterval(() => this.updateDots(), 200);
+  }
+
+  updateDots(): void {
+    this.dots.update((value: number) => (value === 3 ? 1 : value + 1));
+  }
 }
