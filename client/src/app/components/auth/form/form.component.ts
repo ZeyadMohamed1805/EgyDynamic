1d5a17de499ccacb51d79fc18975ea58cabdafc4
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-25 09:17:36 +0300

    Completed The Form Template

--- client/src/app/components/auth/form/form.component.ts
+++ client/src/app/components/auth/form/form.component.ts
@@ -1,12 +1,38 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, NgIf],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrl: './form.component.scss',
 })
 export class FormComponent {
+  formGroup: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {
+    this.formGroup = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.formGroup.value);
+  }
 }
