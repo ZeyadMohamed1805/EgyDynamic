2b664898178b5d85f56285fd85d54376601cf6ee
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-25 06:12:53 +0300

    Added The Pages To The Routing System

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './pages/auth/auth.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [{ component: AuthComponent, path: 'auth' }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '**', component: NotFoundComponent },
+];
