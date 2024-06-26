f92108e22f7b06c61764902ab32e8f25bd5718fc
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-26 09:27:14 +0300

    Initialized The Put Client Component

---
+++ client/src/app/components/clients/put/put.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-put',
+  standalone: true,
+  imports: [],
+  templateUrl: './put.component.html',
+  styleUrl: './put.component.scss'
+})
+export class PutComponent {
+
+}
