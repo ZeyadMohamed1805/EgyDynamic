267cf02984c7f443c30ebc87a4cefdb2c5d0dca7
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2024-06-26 22:18:13 +0300

    Initialized The Calls Component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AuthComponent } from './pages/auth/auth.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ClientsComponent } from './pages/clients/clients.component';
+import { CallsComponent } from './pages/calls/calls.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'auth', component: AuthComponent },
   { path: 'dashboard/clients', component: ClientsComponent },
+  { path: 'dashboard/calls', component: CallsComponent },
   { path: '**', component: NotFoundComponent },
 ];
